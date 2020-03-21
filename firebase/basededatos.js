416d0f48ed6351fb790f575fe77e6121d590880e
duarteissc <duarteissc@users.noreply.github.com>
2020-03-20 18:09:24 -0600

    commit message

---
+++ firebase/basededatos.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCIwrIXOAePrtJ59DB9aJ0urkOSsMXN4YU",
+    authDomain: "proyectojdk66162.firebaseapp.com",
+    databaseURL: "https://proyectojdk66162.firebaseio.com",
+    projectId: "proyectojdk66162",
+    storageBucket: "proyectojdk66162.appspot.com",
+    messagingSenderId: "717498321729",
+    appId: "1:717498321729:web:d059f0ec0915c3141e6fc0"
+  };
+
+// Initialize Firebase LA CONEXIONNNNNNNNNNNNNNNNNNNNNN
+
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
